1e9dbd937965f7b87f05918bea77563be56219f8
mikerbrant <mrbrant@mrbmedia.org>
2018-07-06 12:34:59 -0700

    Logo Image jQuery

--- assets/js/custom-jquery.js
+++ assets/js/custom-jquery.js
@@ -1,14 +1,12 @@
 // Logo Resize
 $(document).on('scroll', function() {
-    logo = "{{ site.logo | absolute_url }}"
-    logoAlt = "{{ site.logoAlt | absolute_url }}"
+    logo = "assets/images/Logo/Logo-White-No-Tagline.svg"
     if ($(document).scrollTop() >= 150) {
       $('#site-logo').css('width', '150px');
       $('#site-logo').css('padding', '0.5em');
-      $('#site-logo').attr('src', logoAlt);
+      $('#site-logo').attr('src', logo);
     } else {
       $('#site-logo').css('width', '');
       $('#site-logo').css('padding', '');
-      $('#site-logo').attr('src', logo);
     }
   });
