286fff4f805d0e4c7ad243b0970b8ef514e98070
mikerbrant <mrbrant@mrbmedia.org>
2018-07-06 12:39:37 -0700

    More Logo Tweaks

--- assets/js/custom-jquery.js
+++ assets/js/custom-jquery.js
@@ -1,12 +1,14 @@
 // Logo Resize
 $(document).on('scroll', function() {
-    logo = "assets/images/Logo/Logo-White-No-Tagline.svg"
+    logo = "assets/images/Logo/Logo-White-Full.svg"
+    logoAlt = "assets/images/Logo/Logo-White-No-Tagline.svg"
     if ($(document).scrollTop() >= 150) {
       $('#site-logo').css('width', '150px');
       $('#site-logo').css('padding', '0.5em');
-      $('#site-logo').attr('src', logo);
+      $('#site-logo').attr('src', logoAlt);
     } else {
       $('#site-logo').css('width', '');
       $('#site-logo').css('padding', '');
+      $('#site-logo').attr('src', logo);
     }
   });
