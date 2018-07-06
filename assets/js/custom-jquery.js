9b8b969221258537235647686942e5d6fd87ec13
mikerbrant <mrbrant@mrbmedia.org>
2018-07-06 12:30:58 -0700

    Testing Logo Swapping

--- assets/js/custom-jquery.js
+++ assets/js/custom-jquery.js
@@ -1,10 +1,14 @@
 // Logo Resize
 $(document).on('scroll', function() {
+    logo = "{{ site.logo | absolute_url }}"
+    logoAlt = "{{ site.logoAlt | absolute_url }}"
     if ($(document).scrollTop() >= 150) {
       $('#site-logo').css('width', '150px');
       $('#site-logo').css('padding', '0.5em');
+      $('#site-logo').attr('src', logoAlt);
     } else {
       $('#site-logo').css('width', '');
       $('#site-logo').css('padding', '');
+      $('#site-logo').attr('src', logo);
     }
   });
