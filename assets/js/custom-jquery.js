2ec32bbf7fca4e64d041131e71f5ddd96b1664ef
mikerbrant <mrbrant@mrbmedia.org>
2018-07-13 23:25:42 -0700

    Update jQuery

--- assets/js/custom-jquery.js
+++ assets/js/custom-jquery.js
@@ -16,13 +16,13 @@ $( document ).ready(function() {
   $(document).on('scroll', function() {
     logo = "https://michaelbrant.com/assets/images/Logo/Logo-White-No-Tagline.svg"  
     logoAlt = "https://michaelbrant.com/assets/images/Logo/Logo-White-Full.svg"
-      if ( $("#header").hasClass("alt") ) {
-        $('#site-logo').css('width', '');
-        $('#site-logo').css('padding', '');
-        $('#site-logo').attr('src', logoAlt);
-      } else {
+      if ( $("#header").hasClass("reveal") ) {
         $('#site-logo').css('width', '150px');
         $('#site-logo').css('padding', '0.5em');
         $('#site-logo').attr('src', logo);
+      } else {
+        $('#site-logo').css('width', '');
+        $('#site-logo').css('padding', '');
+        $('#site-logo').attr('src', logoAlt);
       }
     });    
