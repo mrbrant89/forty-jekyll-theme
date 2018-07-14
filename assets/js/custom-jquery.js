c7d45aad7679f865f68c55c78e70330f3dc8e472
mikerbrant <mrbrant@mrbmedia.org>
2018-07-14 00:05:02 -0700

    jQuery Scroll Logo

--- assets/js/custom-jquery.js
+++ assets/js/custom-jquery.js
@@ -1,4 +1,4 @@
-// Logo Resize On Scroll
+// Determine Which Logo to Load On Load
 $( document ).ready(function() {
   logo = "https://michaelbrant.com/assets/images/Logo/Logo-White-No-Tagline.svg"  
   logoAlt = "https://michaelbrant.com/assets/images/Logo/Logo-White-Full.svg"
@@ -10,5 +10,26 @@ $( document ).ready(function() {
       $("#site-logo").css("width", "150px");
       $("#site-logo").css("padding", "0.5em");
       $("#site-logo").attr("src", logo);
+    }
+  });
+
+// Resize Logo on Scroll  
+  $(document).on('scroll', function() {
+    logo = "https://michaelbrant.com/assets/images/Logo/Logo-White-No-Tagline.svg"  
+    logoAlt = "https://michaelbrant.com/assets/images/Logo/Logo-White-Full.svg"
+    if (($(document).scrollTop() >= 130) && $( "#header" ).hasClass( "alt" )) {
+      $("#site-logo").css("width", "150px");
+      $("#site-logo").css("padding", "0.5em");
+      $("#site-logo").attr("src", logo);
+    }
+  });
+
+  $(document).on('scroll', function() {
+    logo = "https://michaelbrant.com/assets/images/Logo/Logo-White-No-Tagline.svg"  
+    logoAlt = "https://michaelbrant.com/assets/images/Logo/Logo-White-Full.svg"
+    if (($(document).scrollTop() < 130) && $( "#header" ).hasClass( "alt" )) {
+      $('#site-logo').css('width', '');
+      $('#site-logo').css('padding', '');
+      $('#site-logo').attr('src', logoAlt);
     }
   });
