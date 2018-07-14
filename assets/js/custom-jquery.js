836f9046fc8c14680263f002ee64678a4c816d89
mikerbrant <mrbrant@mrbmedia.org>
2018-07-13 21:38:22 -0700

    New Logo jQuery

--- assets/js/custom-jquery.js
+++ assets/js/custom-jquery.js
@@ -1,7 +1,8 @@
 // Logo Resize On Scroll
-$(document).on('scroll', function() {
-    logo = "https://michaelbrant.com/assets/images/Logo/Logo-White-Full.svg"
-    logoAlt = "https://michaelbrant.com/assets/images/Logo/Logo-White-No-Tagline.svg"
+$( document ).ready(function() {
+  logo = "https://michaelbrant.com/assets/images/Logo/Logo-White-No-Tagline.svg"  
+  logoAlt = "https://michaelbrant.com/assets/images/Logo/Logo-White-Full.svg"
+    /*
     if ($(document).scrollTop() >= 150) {
       $('#site-logo').css('width', '150px');
       $('#site-logo').css('padding', '0.5em');
@@ -16,5 +17,15 @@ $(document).on('scroll', function() {
         $('#site-logo').css('padding', '');
         $('#site-logo').attr('src', logo);
       }
+    }
+    */
+    if ( (!$("#header").hasClass("alt")) ) {
+      $('#site-logo').css('width', '150px');
+      $('#site-logo').css('padding', '0.5em');
+      $('#site-logo').attr('src', logoAlt);
+    } else {
+      $('#site-logo').css('width', '');
+      $('#site-logo').css('padding', '');
+      $('#site-logo').attr('src', logo);
     }
   });
