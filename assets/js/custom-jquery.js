5fea9145a77882f699b75b4771719c18e1405f15
mikerbrant <mrbrant@mrbmedia.org>
2018-07-13 23:23:33 -0700

    Update jQuery

--- assets/js/custom-jquery.js
+++ assets/js/custom-jquery.js
@@ -11,10 +11,18 @@ $( document ).ready(function() {
       $('#site-logo').css('padding', '0.5em');
       $('#site-logo').attr('src', logo);
     }
-
-    if ( $("#header").hasClass("reveal") ) {
-      $('#site-logo').css('width', '150px');
-      $('#site-logo').css('padding', '0.5em');
-      $('#site-logo').attr('src', logo);
-    }
   });
+
+  $(document).on('scroll', function() {
+    logo = "https://michaelbrant.com/assets/images/Logo/Logo-White-No-Tagline.svg"  
+    logoAlt = "https://michaelbrant.com/assets/images/Logo/Logo-White-Full.svg"
+      if ( $("#header").hasClass("alt") ) {
+        $('#site-logo').css('width', '');
+        $('#site-logo').css('padding', '');
+        $('#site-logo').attr('src', logoAlt);
+      } else {
+        $('#site-logo').css('width', '150px');
+        $('#site-logo').css('padding', '0.5em');
+        $('#site-logo').attr('src', logo);
+      }
+    });    
